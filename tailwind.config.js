296fc98f5c0728a34e9f7e7b9d1fa03102a477d7
Mario <mperdiguerobarrera@safareyes.es>
2024-11-04 23:21:33 +0100

    🔧 Add spartan configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  presets: [require('@spartan-ng/ui-core/hlm-tailwind-preset')],
   content: [
+    "./libs/**/*.{html,ts}",
     "./src/**/*.{html,ts}",
   ],
   theme: {
