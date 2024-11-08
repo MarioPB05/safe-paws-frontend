5b00e674781c489a1d297bda2bf23670c9150403
Mario <mperdiguerobarrera@safareyes.es>
2024-11-09 00:10:16 +0100

    ✨ Update app root component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,54 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, Input, OnInit} from '@angular/core';
+import {RouteConfigLoadEnd, RouteConfigLoadStart, Router, RouterOutlet} from '@angular/router';
+import {LoadingComponent} from '@shared/components/loading/loading.component';
+import {Observable, tap} from 'rxjs';
+import {AsyncPipe, NgIf} from '@angular/common';
+import {LoadingService} from '@core/services/loading.service';
+import {animate, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoadingComponent, NgIf, AsyncPipe],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  animations: [
+    trigger('fade', [ // Definir la animación
+      transition(':enter', [
+        style({ opacity: 0 }), // Comienza invisible
+        animate('500ms', style({ opacity: 1 })) // Desaparece gradualmente en 500ms
+      ]),
+      transition(':leave', [
+        animate('500ms', style({ opacity: 0 })) // Desaparece gradualmente
+      ])
+    ])
+  ]
 })
-export class AppComponent {
-  title = 'safe-paws-frontend';
+export class AppComponent implements OnInit {
+  loading$: Observable<boolean>;
+
+  @Input()
+  detectRouteTransitions = false;
+
+  constructor(
+    private loadingService: LoadingService,
+    private router: Router
+  ) {
+    this.loading$ = this.loadingService.loading$;
+  }
+
+  ngOnInit() {
+    if (this.detectRouteTransitions) {
+      this.router.events
+        .pipe(
+          tap((event) => {
+            if (event instanceof RouteConfigLoadStart) {
+              this.loadingService.loadingOn();
+            } else if (event instanceof RouteConfigLoadEnd) {
+              this.loadingService.loadingOff();
+            }
+          })
+        )
+        .subscribe();
+    }
+  }
 }
