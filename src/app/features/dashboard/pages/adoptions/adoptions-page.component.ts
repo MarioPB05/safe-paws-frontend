775ff7d0a5c8331dc0cb7224fe16fa6ca20cf8d2
Mario <mperdiguerobarrera@safareyes.es>
2024-11-09 00:09:26 +0100

    🚧 Create Adoptions page

---
+++ src/app/features/dashboard/pages/adoptions/adoptions-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-adoptions-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './adoptions-page.component.html'
+})
+export class AdoptionsPageComponent {
+
+}
