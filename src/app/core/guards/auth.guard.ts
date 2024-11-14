3d0aa8d63c3ecab30998077a65fc03b70de84add
Mario <mperdiguerobarrera@safareyes.es>
2024-11-14 16:33:08 +0100

    ♻️ Refactor auth guard to use new method to check if user is authenticated

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,17 +1,22 @@
 import {CanActivateFn, Router} from '@angular/router';
 import {inject} from '@angular/core';
 import {AuthService} from '@core/services/auth.service';
+import {map} from 'rxjs';
+import {toast} from 'ngx-sonner';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
-  const isAuthenticated = authService.isAuthenticated();
-
-  if (!isAuthenticated) {
-    router.navigate(['/login']);
-    return false;
-  }
-
-  return true;
+  return authService.isAuthenticated().pipe(
+    map((isAuthenticated) => {
+      if (!isAuthenticated) {
+        router.navigate(['/login']).then(() => {
+          toast.error('No tienes una sesión activa, por favor inicia sesión');
+        });
+        return false;
+      }
+      return true;
+    })
+  );
 };
