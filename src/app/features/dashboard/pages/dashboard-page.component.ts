76f8722f83bfa35106cee65acfbda60f361f377a
Mario <mperdiguerobarrera@safareyes.es>
2024-11-15 19:08:44 +0100

    ♻️ Use new navbar

--- src/app/features/dashboard/pages/dashboard-page.component.ts
+++ src/app/features/dashboard/pages/dashboard-page.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {HeaderComponent} from '@dashboard/components/header/header.component';
+import {NavbarComponent} from '@dashboard/components/navbar/navbar.component';
 
 @Component({
   selector: 'app-dashboard-page',
   standalone: true,
   imports: [
     RouterOutlet,
-    HeaderComponent
+    HeaderComponent,
+    NavbarComponent
   ],
   templateUrl: './dashboard-page.component.html'
 })
