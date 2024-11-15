d76724275d5d4f9089cb5e79a94b40fe622cdad6
Miguel Rico <mricofernandez@safareyes.es>
2024-11-15 15:29:31 +0100

    💄 Now it show the components

--- src/app/features/dashboard/pages/dashboard-page.component.ts
+++ src/app/features/dashboard/pages/dashboard-page.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterOutlet} from "@angular/router";
 
 @Component({
   selector: 'app-dashboard-page',
   standalone: true,
-  imports: [],
+    imports: [
+        RouterOutlet
+    ],
   templateUrl: './dashboard-page.component.html'
 })
 export class DashboardPageComponent {
