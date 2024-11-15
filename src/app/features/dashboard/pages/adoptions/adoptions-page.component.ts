627c2fcd586c5fa0967267dbe72d6becbb9f0874
Miguel Rico <mricofernandez@safareyes.es>
2024-11-15 15:27:32 +0100

    🚧 Create adoptions page

--- src/app/features/dashboard/pages/adoptions/adoptions-page.component.ts
+++ src/app/features/dashboard/pages/adoptions/adoptions-page.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {AdoptionCardComponent} from '@dashboard/components/adoption-card/adoption-card.component';
 
 @Component({
   selector: 'app-adoptions-page',
   standalone: true,
-  imports: [],
+  imports: [
+    AdoptionCardComponent
+  ],
   templateUrl: './adoptions-page.component.html'
 })
 export class AdoptionsPageComponent {
