a419380de7c09426e871cdd68b8a73ee4f7c185f
Miguel Rico <mricofernandez@safareyes.es>
2024-11-15 16:33:50 +0100

    💄Add margins and header to dashboard

--- src/app/features/dashboard/pages/dashboard-page.component.ts
+++ src/app/features/dashboard/pages/dashboard-page.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import {RouterOutlet} from "@angular/router";
+import {RouterOutlet} from '@angular/router';
+import {HeaderComponent} from '@dashboard/components/header/header.component';
 
 @Component({
   selector: 'app-dashboard-page',
   standalone: true,
-    imports: [
-        RouterOutlet
-    ],
+  imports: [
+    RouterOutlet,
+    HeaderComponent
+  ],
   templateUrl: './dashboard-page.component.html'
 })
 export class DashboardPageComponent {
