c0f194fdb84b2bd42c0b5f31df3455f012bed844
Miguel Rico <mricofernandez@safareyes.es>
2024-11-20 15:47:45 +0100

    💄Update adoption page to make it responsive and functional

--- src/app/features/dashboard/pages/adoptions/adoptions-page.component.ts
+++ src/app/features/dashboard/pages/adoptions/adoptions-page.component.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AdoptionCardComponent} from '@dashboard/components/adoption-card/adoption-card.component';
+import {AdoptionService} from '@dashboard/services/adoption.service';
+import {AdoptionAvailable} from '@core/models/adoption.model';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-adoptions-page',
   standalone: true,
   imports: [
-    AdoptionCardComponent
+    AdoptionCardComponent,
+    NgForOf
   ],
   templateUrl: './adoptions-page.component.html'
 })
-export class AdoptionsPageComponent {
+export class AdoptionsPageComponent implements OnInit {
+  adoptions: AdoptionAvailable[] = [];
+
+  constructor(private adoptionService: AdoptionService) { }
+
+  ngOnInit() {
+    this.getAdoptions();
+  }
+
+  getAdoptions() {
+    this.adoptionService.getAdoptions().subscribe({
+      next: (adoptions) => {
+        console.log(adoptions);
+        this.adoptions = adoptions;
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    });
+  }
 
 }
