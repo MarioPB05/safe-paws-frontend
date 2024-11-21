e4bd71e96c642ad1a45e69fdfe8ba6b0cda921ef
Miguel Rico <mricofernandez@safareyes.es>
2024-11-21 16:40:07 +0100

    💄 Manage errors adding alerts and toasts

--- src/app/features/dashboard/pages/adoptions/adoptions-page.component.ts
+++ src/app/features/dashboard/pages/adoptions/adoptions-page.component.ts
@@ -2,19 +2,37 @@ import {Component, OnInit} from '@angular/core';
 import {AdoptionCardComponent} from '@dashboard/components/adoption-card/adoption-card.component';
 import {AdoptionService} from '@dashboard/services/adoption.service';
 import {AdoptionAvailable} from '@core/models/adoption.model';
-import {NgForOf} from '@angular/common';
+import {NgForOf, NgIf} from '@angular/common';
+import {toast} from 'ngx-sonner';
+import {HlmIconComponent} from '@spartan-ng/ui-icon-helm';
+import {provideIcons} from '@ng-icons/core';
+import {lucideAlertTriangle} from '@ng-icons/lucide';
+import {
+  HlmAlertDescriptionDirective, HlmAlertDirective,
+  HlmAlertIconDirective,
+  HlmAlertTitleDirective
+} from '@spartan-ng/ui-alert-helm';
+
 
 @Component({
   selector: 'app-adoptions-page',
   standalone: true,
   imports: [
     AdoptionCardComponent,
-    NgForOf
+    NgForOf,
+    NgIf,
+    HlmIconComponent,
+    HlmAlertDescriptionDirective,
+    HlmAlertTitleDirective,
+    HlmAlertIconDirective,
+    HlmAlertDirective
   ],
-  templateUrl: './adoptions-page.component.html'
+  templateUrl: './adoptions-page.component.html',
+  providers: [provideIcons({ lucideAlertTriangle })]
 })
 export class AdoptionsPageComponent implements OnInit {
   adoptions: AdoptionAvailable[] = [];
+  error: boolean = false;
 
   constructor(private adoptionService: AdoptionService) { }
 
@@ -25,11 +43,15 @@ export class AdoptionsPageComponent implements OnInit {
   getAdoptions() {
     this.adoptionService.getAdoptions().subscribe({
       next: (adoptions) => {
-        console.log(adoptions);
         this.adoptions = adoptions;
+        if(this.adoptions.length === 0) {
+          this.error = true;
+          toast.error('No hay adopciones disponibles')
+        }
       },
       error: (err) => {
-        console.error(err);
+        this.error = true;
+        toast.error('Hubo un error al cargar las adopciones');
       }
     });
   }
