4bdd29f0bca95800244c683031abd73c15bddee8
Mario <mperdiguerobarrera@safareyes.es>
2024-11-21 16:06:19 +0100

    ♻️ Remove HTTP context

--- src/app/features/dashboard/services/adoption.service.ts
+++ src/app/features/dashboard/services/adoption.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpContext} from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {AdoptionAvailable} from '@core/models/adoption.model';
-import {SkipLoading} from '@core/interceptors/loading.interceptor';
 
 @Injectable({
   providedIn: 'root'
@@ -12,8 +11,6 @@ export class AdoptionService {
   constructor(private http: HttpClient) { }
 
   getAdoptions(): Observable<AdoptionAvailable[]> {
-    return this.http.get<AdoptionAvailable[]>('/api/adoptions/',  {
-      context: new HttpContext().set(SkipLoading, true) // TODO Si se pone el loading da error
-    });
+    return this.http.get<AdoptionAvailable[]>('/api/adoptions/');
   }
 }
