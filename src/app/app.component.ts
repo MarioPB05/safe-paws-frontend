3eff7a60c7d918851172758b97c0bcb47f646061
Mario <mperdiguerobarrera@safareyes.es>
2024-11-21 16:04:08 +0100

    🐛 Fix visual bug when loading is active

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,6 @@ import {LoadingComponent} from '@shared/components/loading/loading.component';
 import {Observable, tap} from 'rxjs';
 import {AsyncPipe, NgIf} from '@angular/common';
 import {LoadingService} from '@core/services/loading.service';
-import {animate, style, transition, trigger} from '@angular/animations';
 import {HlmToasterComponent} from '@spartan-ng/ui-sonner-helm';
 
 @Component({
@@ -12,17 +11,6 @@ import {HlmToasterComponent} from '@spartan-ng/ui-sonner-helm';
   standalone: true,
   imports: [RouterOutlet, LoadingComponent, NgIf, AsyncPipe, HlmToasterComponent],
   templateUrl: './app.component.html',
-  animations: [
-    trigger('fade', [ // Definir la animación
-      transition(':enter', [
-        style({ opacity: 0 }), // Comienza invisible
-        animate('500ms', style({ opacity: 1 })) // Desaparece gradualmente en 500ms
-      ]),
-      transition(':leave', [
-        animate('500ms', style({ opacity: 0 })) // Desaparece gradualmente
-      ])
-    ])
-  ]
 })
 export class AppComponent implements OnInit {
   loading$: Observable<boolean>;
