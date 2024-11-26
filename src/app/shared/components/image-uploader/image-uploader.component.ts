ae77ba5b0d76c8b37087630dd6b79fd1b7d9927b
Mario <mperdiguerobarrera@safareyes.es>
2024-11-26 22:55:42 +0100

    ✨ Add default image

--- src/app/shared/components/image-uploader/image-uploader.component.ts
+++ src/app/shared/components/image-uploader/image-uploader.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {NgStyle} from '@angular/common';
 
 @Component({
@@ -9,14 +9,23 @@ import {NgStyle} from '@angular/common';
   ],
   templateUrl: './image-uploader.component.html'
 })
-export class ImageUploaderComponent {
+export class ImageUploaderComponent implements OnInit {
   @Input() svgSize: string = 'size-32';
   @Input() textSize: string = 'text-lg';
+  @Input() defaultImage: string | null = null;
 
   @Output() imageSelected = new EventEmitter<File>();
 
   previewUrl: string | ArrayBuffer | null = '';
   showPreview = false;
+
+  ngOnInit(): void {
+    if (this.defaultImage) {
+      console.log('defaultImage', this.defaultImage);
+      this.previewUrl = this.defaultImage;
+      this.showPreview = true;
+    }
+  }
 
   onFileSelected(event: Event): void {
     const input = event.target as HTMLInputElement;
