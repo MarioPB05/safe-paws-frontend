9b56c59b08b55415118b8cfb75b076f1bf8e57f1
Mario <mperdiguerobarrera@safareyes.es>
2024-11-26 22:25:36 +0100

    🔥 Remove unnecessary imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,12 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {RouteConfigLoadEnd, RouteConfigLoadStart, Router, RouterOutlet} from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 import {LoadingComponent} from '@shared/components/loading/loading.component';
-import {Observable, tap} from 'rxjs';
-import {AsyncPipe, NgIf} from '@angular/common';
-import {LoadingService} from '@core/services/loading.service';
 import {HlmToasterComponent} from '@spartan-ng/ui-sonner-helm';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoadingComponent, NgIf, AsyncPipe, HlmToasterComponent],
+  imports: [RouterOutlet, LoadingComponent, HlmToasterComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
