46a2915a5d00d14cbc2d141494ed580a09f03aca
Mario <mperdiguerobarrera@safareyes.es>
2024-11-27 10:10:10 +0100

    ♻️ Add context to skip token injection

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -1,10 +1,16 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import {HttpContextToken, HttpInterceptorFn} from '@angular/common/http';
 import {inject} from '@angular/core';
 import { AuthService } from '@core/services/auth.service';
+
+export const SkipAuth = new HttpContextToken<boolean>(() => false);
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService);
   const token = authService.getToken();
+
+  if (req.context.get(SkipAuth)) {
+    return next(req);
+  }
 
   const authReq = token
     ? req.clone({ setHeaders: { Authorization: `Bearer ${token}` } })
