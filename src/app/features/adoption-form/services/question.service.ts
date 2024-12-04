7b0434af36096c564a4126e369d2268de3f8677e
Miguel Rico <mricofernandez@safareyes.es>
2024-12-04 20:38:30 +0100

    ✨ Create question service

---
+++ src/app/features/adoption-form/services/question.service.ts
@@ -0,0 +1,16 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {QuestionsAvailable} from '@core/models/question.model';
+import {Observable} from 'rxjs';
+
+@Injectable(
+    {providedIn: 'root'}
+)
+export class QuestionService {
+
+  constructor(private http: HttpClient) { }
+
+  getQuestions(): Observable<QuestionsAvailable[]> {
+    return this.http.get<QuestionsAvailable[]>('/api/questions/getQuestions');
+  }
+}
