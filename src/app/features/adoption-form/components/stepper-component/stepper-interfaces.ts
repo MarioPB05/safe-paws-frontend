6b0ce57d4bcc2a0a779f30ac571a3269eba24490
Mario <mperdiguerobarrera@safareyes.es>
2024-12-05 13:38:48 +0100

    ✨ Create stepper interface

---
+++ src/app/features/adoption-form/components/stepper-component/stepper-interfaces.ts
@@ -0,0 +1,9 @@
+import {TemplateRef} from '@angular/core';
+
+export interface Step {
+  title: string;
+  description: string;
+  isActive: boolean;
+  isCompleted: boolean;
+  content: TemplateRef<any>;
+}
