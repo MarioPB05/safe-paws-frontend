3caf8896c2081a230fd6cec4e86ed43b5e3d072d
Mario <mperdiguerobarrera@safareyes.es>
2024-12-10 01:00:22 +0100

    ✨ Finish adoption tracking page

--- src/app/features/adoption-tracking/adoption-tracking-page.component.ts
+++ src/app/features/adoption-tracking/adoption-tracking-page.component.ts
@@ -17,8 +17,16 @@ import {
 import {BrnAlertDialogContentDirective, BrnAlertDialogTriggerDirective} from '@spartan-ng/ui-alertdialog-brain';
 // @ts-ignore
 import confetti from 'canvas-confetti';
-import {AuthService} from '@core/services/auth.service';
 import {ChatComponent} from '@shared/components/chat/chat.component';
+import {NgIf} from '@angular/common';
+import {BrnDialogContentDirective, BrnDialogTriggerDirective} from '@spartan-ng/ui-dialog-brain';
+import {SignaturePadComponent} from '@shared/components/signature-pad/signature-pad.component';
+import {
+  HlmDialogComponent,
+  HlmDialogContentComponent, HlmDialogDescriptionDirective,
+  HlmDialogFooterComponent,
+  HlmDialogHeaderComponent, HlmDialogTitleDirective
+} from '@spartan-ng/ui-dialog-helm';
 
 @Component({
   imports: [
@@ -38,6 +46,16 @@ import {ChatComponent} from '@shared/components/chat/chat.component';
     BrnAlertDialogContentDirective, // El IDE no reconoce esta directiva, pero se está usando
     BrnAlertDialogTriggerDirective,
     ChatComponent,
+    NgIf,
+    BrnDialogTriggerDirective,
+    SignaturePadComponent,
+    BrnDialogContentDirective,
+    HlmDialogComponent,
+    HlmDialogContentComponent,
+    HlmDialogHeaderComponent,
+    HlmDialogFooterComponent,
+    HlmDialogTitleDirective,
+    HlmDialogDescriptionDirective,
   ],
   selector: 'app-adoption-tracking-page',
   standalone: true,
@@ -48,15 +66,18 @@ export class AdoptionTrackingPageComponent implements OnInit {
   @ViewChild('stepTwo') stepTwo!: TemplateRef<any>;
   @ViewChild('stepThree') stepThree!: TemplateRef<any>;
   @ViewChild(StepperComponentComponent) stepper!: StepperComponentComponent;
+  @ViewChild(HlmDialogComponent) dialog!: HlmDialogComponent;
+  @ViewChild(HlmAlertDialogComponent) alertDialog!: HlmAlertDialogComponent;
+  @ViewChild(SignaturePadComponent) signaturePad!: SignaturePadComponent;
 
   steps: Step[] = [];
   requestCode: string = '';
+  chatRoomCode: string = '';
   status: number = 0;
 
   constructor(
     private route: ActivatedRoute,
     private requestService: RequestService,
-    private authService: AuthService,
     private router: Router
   ) {}
 
@@ -66,36 +87,24 @@ export class AdoptionTrackingPageComponent implements OnInit {
 
       this.requestService.getRequestStatus(this.requestCode).subscribe({
         next: (requestStatusResponse) => {
-          const { status } = requestStatusResponse;
+          const { status, chatRoomCode } = requestStatusResponse;
 
           const stepOne = status === 0;
           const stepTwo = status === 1 || status === 2 || status === 3;
-          const stepThree = status === 4;
+          const stepThree = status === 4 || status === 5;
           const finished = status === 5;
 
           this.status = status;
 
-          if (status === 2) {
-            const duration = 15 * 1000;
-            const animationEnd = Date.now() + duration;
-            const defaults = {startVelocity: 30, spread: 360, ticks: 60, zIndex: 0};
-
-            function randomInRange(min: number, max: number) {
-              return Math.random() * (max - min) + min;
+          if ([2,4,5].includes(status)) {
+            if (chatRoomCode != undefined) {
+              this.chatRoomCode = chatRoomCode;
             }
 
-            const interval = setInterval(function () {
-              const timeLeft = animationEnd - Date.now();
-
-              if (timeLeft <= 0) {
-                return clearInterval(interval);
-              }
-
-              const particleCount = 50 * (timeLeft / duration);
-              // since particles fall down, start a bit higher than random
-              confetti({...defaults, particleCount, origin: {x: randomInRange(0.1, 0.3), y: Math.random() - 0.2}});
-              confetti({...defaults, particleCount, origin: {x: randomInRange(0.7, 0.9), y: Math.random() - 0.2}});
-            }, 250);
+            if (status === 2) {
+              this.confetti();
+              localStorage.setItem('confettiShown', 'true');
+            }
           }
 
           setTimeout(() => {
@@ -134,10 +143,73 @@ export class AdoptionTrackingPageComponent implements OnInit {
     });
   }
 
+  confetti() {
+    if (localStorage.getItem('confettiShown')) return;
+
+    const duration = 15 * 1000;
+    const animationEnd = Date.now() + duration;
+    const defaults = {startVelocity: 30, spread: 360, ticks: 60, zIndex: 0};
+
+    function randomInRange(min: number, max: number) {
+      return Math.random() * (max - min) + min;
+    }
+
+    const interval = setInterval(function () {
+      const timeLeft = animationEnd - Date.now();
+
+      if (timeLeft <= 0) {
+        return clearInterval(interval);
+      }
+
+      const particleCount = 50 * (timeLeft / duration);
+      // since particles fall down, start a bit higher than random
+      confetti({...defaults, particleCount, origin: {x: randomInRange(0.1, 0.3), y: Math.random() - 0.2}});
+      confetti({...defaults, particleCount, origin: {x: randomInRange(0.7, 0.9), y: Math.random() - 0.2}});
+    }, 250);
+  }
+
   downloadPdf() {
     if (!this.requestCode) return;
 
     this.requestService.downloadRequestPdf(this.requestCode);
+  }
+
+  downloadContract() {
+    toast.info('Descargando contrato de adopción', { description: 'El contrato se descargará automáticamente' });
+    this.requestService.downloadAdoptionContract(this.requestCode);
+  }
+
+  closeAll() {
+    this.dialog.close(1);
+    this.alertDialog.close(1);
+  }
+
+  checkSignature() {
+    if (this.signaturePad.isCanvasBlank()) {
+      toast.error('Firma no encontrada', { description: 'Por favor, firma el contrato antes de continuar' });
+      return;
+    }
+
+    this.alertDialog.open();
+  }
+
+  signContract() {
+    if (!this.requestCode) return;
+
+    this.closeAll();
+
+    this.requestService.signContract(this.requestCode, { signature: this.signaturePad.getImage(), isOwner: false }).subscribe({
+      next: () => {
+        this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+          this.router.navigate(['/adoption/tracking/' + this.requestCode]).then(() => {
+            toast.info('Has firmado el contrato de adopción', { description: 'Ahora puedes descargar el contrato' });
+          });
+        });
+      },
+      error: () => {
+        toast.error('Ha ocurrido un error al firmar el contrato de adopción');
+      }
+    });
   }
 
 }
